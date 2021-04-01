41043dc80f723dd53960009ffadb82ede052443f
Seongmin Choi <alex2bmp@gmail.com>
2021-04-01 23:35:31 +0900

    added alarm code and run script

---
+++ run_alarm.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /home/smchoi/py38/bin/activate
+date
+python /home/smchoi/projects/sentiment_alarm/sentiment_alarm.py
---
+++ sentiment_alarm.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+from datetime import datetime
+import slack
+from pathlib import Path
+from dotenv import load_dotenv
+from slack import WebClient
+from slack.errors import SlackApiError
+
+# Parse FnG history
+url = "https://alternative.me/crypto/fear-and-greed-index/#fng-history"
+png_url = "https://alternative.me/crypto/fear-and-greed-index/#fng-history"
+page = requests.get(url)
+text = page.text
+soup = BeautifulSoup(page.content, 'html.parser')
+
+fngs = soup.find_all('div', class_='fng-circle')
+
+fng_scores = []
+for fng in fngs:
+    fng_score = fng.text
+    fng_scores.append(fng_score)
+
+out_text = ("Today: %s <- Yesterday: %s (link: %s)" 
+            % (fng_scores[0], fng_scores[1], png_url))
+#print(out_text)
+
+
+# Check log
+base_dir = "/home/smchoi/projects/sentiment_alarm"
+log_data = {}
+log_file = '%s/messages.log' % base_dir
+if os.path.exists(log_file):
+    for line in open(log_file, 'r'):
+        field = line.rstrip().split('\t')
+        date, value = field
+        log_data[date] = value
+today = datetime.today().strftime('%Y-%m-%d')
+
+
+# Send slack messages
+if today not in log_data:
+    env_path = Path(base_dir) / '.env'
+    load_dotenv(dotenv_path=env_path)
+    
+    slack_token = os.environ['SLACK_TOKEN']
+    client = WebClient(token=slack_token)
+    
+    try:
+        response = client.chat_postMessage(
+            channel="#coin_bug",
+            text=out_text # FnG
+        )
+        with open(log_file, 'a') as fp:
+            fp.write('%s\t%s\n' % (today, out_text))
+
+    except SlackApiError as e:
+        # You will get a SlackApiError if "ok" is False
+        assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
+
+else:
+    print('%s data has already been saved in %s' % (today, log_file))
